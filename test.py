614a670a61a0cd06a7682dce2f121e288f4534a7
celovek097 <loshka186@gmail.com>
2026-03-22 18:28:06 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -14,15 +14,19 @@ from os import path
 path = path.dirname(path.abspath(__file__))+"/models/vosk-model-small-ru-0.22" 
 model = Model(path) #путь до модели обработчика голоса Vosk
 
-system_instruction = "Ты Стелла, голосовой ассистент, общайся с пользователем как с другом, всегда отвечай на русском и только буквами, всегда укладывай ответ в 1000 символов."
+print("LOG Load system_instruction")
+system_instruction = "Ты Стелла, голосовой ассистент, общайся с пользователем как с другом, всегда отвечай на русском и не используй смайлики или иные символы которые не способен обработать синтезатор речи, всегда укладывай ответ в 1000 символов."
 response: ChatResponse = chat(model='gemma3', messages=[{'role': 'system', 'content': system_instruction,}])
 print(response.message.content)
 
 recognizer = KaldiRecognizer(model, 16000)
 
+print("LOG load listening setting")
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000,input=True, frames_per_buffer=4000)
 
+#Инициализация silero
+print("LOG load silero, tts module")
 device = torch.device('cpu')  #'cpu' можно и 'cuda' если есть GPU
 model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                      model='silero_tts',
@@ -49,16 +53,17 @@ while True:
 			if "выход" in text:
 				print("Exiting program...")
 				break
-			response: ChatResponse = chat(model='gemma3', messages=[{'role': 'user', 'content': text,}]) #Запрос
+			elif text != "" or text != " ":
+				response: ChatResponse = chat(model='gemma3', messages=[{'role': 'user', 'content': text,}]) #Запрос
 
-			text = response.message.content #Результат
-			print(text)
+				text = response.message.content #Результат
+				print(text)
 			
 			#speak(text)
 			# Генерируем аудио и воспроизводим аудио Доступные голоса: ['aidar', 'baya', 'kseniya', 'xenia', 'eugene', 'random']
-			audio = model.apply_tts(text=text,speaker='kseniya', sample_rate=48000)
-			sd.play(audio, 48000)
-			sd.wait()
+				audio = model.apply_tts(text=text,speaker='kseniya', sample_rate=48000)
+				sd.play(audio, 48000)
+				sd.wait()
 
 	#except sr.RequestError as e:
 		#print("Could not request results; {0}".format(e))
