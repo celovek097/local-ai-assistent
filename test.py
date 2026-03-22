60cabfa17f65e00f6e2677833e0b7e550ed421f8
celovek097 <loshka186@gmail.com>
2026-03-22 18:47:55 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from ollama import chat
-from ollama import ChatResponse
+from ollama import chat, ChatResponse
 import sys
 import json
 import pyaudio
@@ -10,14 +9,38 @@ from os import path
 #import soundfile as sf
 #from features.speak import speak
 #from pathlib import Path
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="sk-a03cec19ea88493c85fb3a3e0c8592e0",
+    base_url="https://api.deepseek.com"
+)
+
+def ethernet_response(text):
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "Ты "},
+            {"role": "user", "content": text},
+        ],
+        stream=False
+    )
+    return response.choices[0].message.content
+
+def local_response(text):
+    response: ChatResponse = chat(
+        model='gemma3',#Запрос
+        messages=[
+            {'role': 'system', 'content': text}])
+    return response.message.content #Результат
+
 
 path = path.dirname(path.abspath(__file__))+"/models/vosk-model-small-ru-0.22" 
 model = Model(path) #путь до модели обработчика голоса Vosk
 
 print("LOG Load system_instruction")
-system_instruction = "Ты Стелла, голосовой ассистент, общайся с пользователем как с другом, всегда отвечай на русском и не используй смайлики или иные символы которые не способен обработать синтезатор речи, всегда укладывай ответ в 1000 символов."
-response: ChatResponse = chat(model='gemma3', messages=[{'role': 'system', 'content': system_instruction,}])
-print(response.message.content)
+system_instruction = "Ты Стелла, голосовой ассистент, общайся с пользователем как с другом, всегда отвечай на русском и только буквами, всегда укладывай ответ в 1000 символов."
+print(local_response(system_instruction))
 
 recognizer = KaldiRecognizer(model, 16000)
 
@@ -25,7 +48,6 @@ print("LOG load listening setting")
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000,input=True, frames_per_buffer=4000)
 
-#Инициализация silero
 print("LOG load silero, tts module")
 device = torch.device('cpu')  #'cpu' можно и 'cuda' если есть GPU
 model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
@@ -41,32 +63,24 @@ print(f"Доступные голоса: {available_speakers}")
 
 
 while True:
-	try:
-		data = stream.read(8000, exception_on_overflow=False)
-		print("Listening...")
-		if recognizer.AcceptWaveform(data): #Обработка звука блоками по 4000 байт (или бит не помню)
-			result_dict = json.loads(recognizer.Result())
-			text = result_dict.get("text", "")
-			text = text.lower()  
-			print("You said:", text)
+    try:
+        data = stream.read(8000, exception_on_overflow=False)
+        print("Listening...")
+        if recognizer.AcceptWaveform(data): #Обработка звука блоками по 4000 байт (или бит не помню)
+            result_dict = json.loads(recognizer.Result())
+            text = result_dict.get("text", "")
+            text = text.lower()
+            print("You said:", text)
 
-			if "выход" in text:
-				print("Exiting program...")
-				break
-			elif text != "" or text != " ":
-				response: ChatResponse = chat(model='gemma3', messages=[{'role': 'user', 'content': text,}]) #Запрос
-
-				text = response.message.content #Результат
-				print(text)
-			
-			#speak(text)
+            if "выход" in text:
+                print("Exiting program...")
+                break
+            text = local_response(text)
+            print(text)
+            #speak(text)
 			# Генерируем аудио и воспроизводим аудио Доступные голоса: ['aidar', 'baya', 'kseniya', 'xenia', 'eugene', 'random']
-				audio = model.apply_tts(text=text,speaker='kseniya', sample_rate=48000)
-				sd.play(audio, 48000)
-				sd.wait()
-
-	#except sr.RequestError as e:
-		#print("Could not request results; {0}".format(e))
-
-	except sr.UnknownValueError:
-		print("Could not understand audio")
+            audio = model.apply_tts(text=text,speaker='kseniya', sample_rate=48000)
+            sd.play(audio, 48000)
+            sd.wait()
+    except:
+        print("err")
