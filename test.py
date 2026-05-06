64b0461d7146a8ae08a18ccf9cf108e794162b21
celovek097 <loshka186@gmail.com>
2026-05-06 12:16:35 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,39 +1,39 @@
 from ollama import chat, ChatResponse
-import sys
+from cerebras.cloud.sdk import Cerebras
+from vosk import Model, KaldiRecognizer
+import os
+import dotenv
 import json
 import pyaudio
-#from piper import PiperVoice
-import numpy as np
-from vosk import Model, KaldiRecognizer
 import torch
 import sounddevice as sd
-from os import path
-#import soundfile as sf
-#from features.speak import speak
-#from pathlib import Path
+import socket
 
+dotenv.load_dotenv()  # Загружаем переменные из .env
+api_key = os.getenv("API_KEY")
+client = Cerebras(api_key=api_key)  # Клиент сам найдёт ключ
 local_ai_model = "gemma3:latest"
+ethernet_ai_model = "llama3.1-8b"
 system_instruction = "Ты Стелла, голосовой ассистент, общайся с пользователем как с другом, поддерживай простой диалог, всегда отвечай на русском и только буквами, всегда укладывай ответ в 1000 символов."
 
 sys_instr = [{'role': 'system', 'content': system_instruction}, {'role': 'assistant', 'content': 'Здравствуйте!'}]
 
-#history = []
 def clear_history(filename="history.txt"):
 	try:
-		abs_path = path.dirname(path.abspath(__file__))+'/'+filename
+		abs_path = os.path.dirname(os.path.abspath(__file__))+'/'+filename
 		with open(abs_path, 'w', encoding='utf-8'):
-			print(' -------------\n',' clear history success\n','-------------')
+			print(' -----------------------\n',' clear history success\n','-----------------------')
 	except:
 		print("error clear history")
 
 def save_history(history, filename="history.txt"):
-	abs_path = path.dirname(path.abspath(__file__))+'/'+filename
+	abs_path = os.path.dirname(os.path.abspath(__file__))+'/'+filename
 	with open(abs_path, 'a', encoding='utf-8') as f:
 		json.dump(history, f, ensure_ascii=False)
 		f.write('\n')
 
 def load_history(filename="history.txt"):
-	abs_path = path.dirname(path.abspath(__file__))+'/'+filename
+	abs_path = os.path.dirname(os.path.abspath(__file__))+'/'+filename
 	try:
 		with open(abs_path, 'r', encoding='utf-8') as  f:
 			content = []
@@ -59,17 +59,35 @@ def local_response(text, role="user"):
 	save_history({'role':'assistant', 'content':result})
 	return result #Результат
 
-#def speak(text=''):
-#	voice = PiperVoice.load(path.dirname(path.abspath(__file__))+"/ru_RU-irina-medium.onnx")
-#	with sd.RawOutputStream(samplerate=voice.config.sample_rate, channels=1, dtype='int16') as stream:
-#		#text = "Привет! Это пример потокового воспроизведения текста в реальном времени. Этот метод позволяет начать слышать голос еще до того, как вся фраза будет полностью синтезирована."    
-#		for audio_bytes in voice.synthesize_stream_raw(text):
-		# Преобразуем байты в numpy массив (int16), как требует sounddevice
-		# Если используете GLaDOSify, он умеет обрабатывать чанки любой длины
-#			audio_block = np.frombuffer(audio_bytes, dtype=np.int16)
-#			stream.write(audio_block)  # Отправляем блок на воспроизведение
+def ethernet_response(text, role="user"):
+	global ethernet_ai_model, sys_instr
+	history = load_history()
+	message ={'role': role, 'content': text}
+	content = [*sys_instr, *history, message] if history != None else [sys_instr, message]
+	completion = client.chat.completions.create(messages=content,model=ethernet_ai_model)
+	save_history(message)
+	print(content, '\n')
+	result = completion.choices[0].message.content
+	save_history({'role':'assistant', 'content':result})
+	return result #Результат
 
-vosk_path = path.dirname(path.abspath(__file__))+"/models/vosk-model-small-ru-0.22" 
+def check_internet(host="8.8.8.8", port=53, timeout=3):
+	print("checking internet connection")
+	"""
+	Проверка доступа в интернет через создание сокета.
+	По умолчанию использует DNS-сервер Google (8.8.8.8) и порт 53.
+	"""
+	try:
+		# Устанавливаем соединение, таймаут - 3 секунды
+		socket.setdefaulttimeout(timeout)
+		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+		print("True")
+		return True
+	except socket.error:
+		print("False")
+		return False
+
+vosk_path = os.path.dirname(os.path.abspath(__file__))+"/models/vosk-model-small-ru-0.22"
 vosk_model = Model(vosk_path) #stt путь до модели обработчика голоса Vosk
 
 recognizer = KaldiRecognizer(vosk_model, 16000)
@@ -82,9 +100,9 @@ print("LOG load silero, tts module")
 
 device = torch.device('cpu')  #'cpu' можно и 'cuda' если есть GPU
 model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                     model='silero_tts',
-                                     language='ru',
-                                     speaker='v3_1_ru')  # или 'v4_ru' для новой версии
+									 model='silero_tts',
+									 language='ru',
+									 speaker='v3_1_ru')  # или 'v4_ru' для новой версии
 
 model.to(device)
 
@@ -109,9 +127,9 @@ while True:
 				stream.close()
 				break
 			if text != "" and text != " ":
-				text = local_response(text)
+				if check_internet: text = ethernet_response(text)
+				else:text = local_response(text)
 				print(text)
-				#speak(text)
 				# Генерируем аудио и воспроизводим аудио Доступные голоса: ['aidar', 'baya', 'kseniya', 'xenia', 'eugene', 'random']
 				audio = model.apply_tts(text=text,speaker='xenia', sample_rate=48000)
 				sd.play(audio, samplerate=48000, latency='low', blocksize=256)
